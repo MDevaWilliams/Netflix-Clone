7a2245d759a19487798a2d4ffb2285f5dc71b946
MDevaWilliams <107609876+MDevaWilliams@users.noreply.github.com>
2025-08-27 22:18:07 +0000

    Removed all ignored files from tracking

--- Netflix/Netflix/settings.py
+++ Netflix/Netflix/settings.py
@@ -158,9 +158,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-
-
-
 # --------- Fetch S3 Secrets ----------
 AWS_S3_BUCKET_NAME = get_parameter("/netflix/S3/BUCKET_NAME")
 AWS_S3_BUCKET_REGION = get_parameter("/netflix/S3/BUCKET_REGION")
